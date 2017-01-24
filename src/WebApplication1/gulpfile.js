22a468ff9f1aefe505b6925f84a06f431c9bb7a4
David Nachman <david@nachman.org>
2017-01-23 21:30:58 -0500

    added grunt and gulp

---
+++ src/WebApplication1/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/// <binding AfterBuild='stylemaker' ProjectOpened='lesswatcher' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+
+var gulp = require('gulp');
+var gulpless = require('gulp-less');
+
+gulp.task('stylemaker', function () {
+    gulp.src("./less/styles.less")
+        .pipe(gulpless({ compress: true }))
+        .pipe(gulp.dest("./wwwroot/css"));
+});
+
+gulp.task('lesswatcher', function() {
+    gulp.watch("./less/*.less", ["stylemaker"]);
+});
